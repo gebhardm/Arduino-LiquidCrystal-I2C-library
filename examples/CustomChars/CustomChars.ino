1fd0715d2c4a7af1a86064ff1f52704058b54312
Markus Gebhard <markus.gebhard@web.de>
2018-06-03 16:12:31 +0200

    adapt examples to Pollin I2C all low address

--- examples/CustomChars/CustomChars.ino
+++ examples/CustomChars/CustomChars.ino
@@ -11,7 +11,7 @@ uint8_t cross[8] = {0x0, 0x1b, 0xe, 0x4, 0xe, 0x1b, 0x0};
 uint8_t retarrow[8] = {	0x1, 0x1, 0x5, 0x9, 0x1f, 0x8, 0x4};
 
 // Set the LCD address to 0x27 for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+LiquidCrystal_I2C lcd(0x20, 16, 2); // set Pollin I2C all low address
 
 void setup()
 {
@@ -46,7 +46,7 @@ void displayKeyCodes(void) {
 		lcd.print("Codes 0x");
 		lcd.print(i, HEX);
 		lcd.print("-0x");
-		lcd.print(i + 16, HEX);
+		lcd.print(i + 15, HEX);
 		lcd.setCursor(0, 1);
 
 		for (int j = 0; j < 16; j++) {
